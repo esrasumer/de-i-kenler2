0bc358e32eec321e18dd7f373d0032bd15fd02cf
esrasumer <esrasumer28@gmail.com>
2022-05-20 00:45:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//değişkenler //ctrl+k+c
+
+// let age;
+// age = 20;
+// console.log(age);
+
+// let fullname = "Esra";
+// fullname = "Esra sümer";
+
+// console.log(fullname);
+
+// //değişken tanımlama kuralları 
+
+// //1. sayısal ifade başlayamaz
+
+// //komut isimleriyle tanımlama yapılmaz
+
+// let city = "İstanbul";
+// console.log(city);
+
+let val;
+
+val = String(new Date());
+
+console.log(val);
